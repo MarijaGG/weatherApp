0ad257935729a3290e64ab4ff7041545a6db9f70
MarijaGG <ipa23.m.gutane@vtdt.edu.lv>
2025-09-08 13:47:28 +0300

    api strada yyey

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php 
 
 
-
-
+$data = file_get_contents("https://emo.lv/weather-api/forecast/?city=cesis,latvia");
+$weather = json_decode($data, true);
 
 ?>
+<html>
+<body>
+
+<p>Pilsēta: <?php echo $weather['city']['name']; ?></p>
+
+<p>Temperatūra: <?php echo $weather['list'][0]['temp']['day']; ?>°C<br>
+   Feels like: <?php echo $weather['list'][0]['feels_like']['day']; ?>°C</p>
+
+
+</body>
+</html>
